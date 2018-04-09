fcbb176c0372c3fb0f5ad9b9006cfc38ca67672b
Spencer Dent <spencer.dent@covenant.edu>
2018-04-09 15:37:40 -0400

    connects node server to main page

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var app = express();
 
 app.get('/', function(req, res) {
   console.log("Someone's here!");
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/warship.html');
 });
 
 app.get('/styles', function(req, res) {
-  res.sendFile(__dirname + '/styles.css');
+  res.sendFile(__dirname + '/style.css');
 })
 
 app.listen(8012);
