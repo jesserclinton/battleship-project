5fc92967c4e5a0ef6e6d0ed7756e25cd6d125f36
Spencer Dent <spencer.dent@covenant.edu>
2018-04-10 17:11:48 -0400

    connect files on server

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/warship.html');
 });
 
-app.get('/styles', function(req, res) {
+app.get('/style.css', function(req, res) {
   res.sendFile(__dirname + '/style.css');
+})
+
+app.get('/script.js', function(req, res) {
+  res.sendFile(__dirname + '/script.js');
 })
 
 app.listen(8012);
