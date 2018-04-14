b911409ff6266a60d0a51a2689306f8a9248f0a5
Spencer Dent <spencer.dent@covenant.edu>
2018-04-14 02:16:15 -0400

    changes build functions to use jQuery

--- main.js
+++ main.js
@@ -1,94 +1,86 @@
-//-----Variables-----
-var players = [];
-
-//-----Settings-----
+//=====Settings=====
 onsubmit = function(e) {
   e.preventDefault();
 }
 
+//=====Functions=====
 //-----Login-----
-function buildLogins() {
+function buildLogins(players) {
   // console.log(players);
   var section = $('#players');
   section.empty();
-  var ol = document.createElement('ol');
+  var ol = $(document.createElement('ol'));
   for (player of players) {
-    var li = document.createElement('li');
-    li.innerHTML = player.name;
-    ol.appendChild(li);
+    $(document.createElement('li')).text(player.name).appendTo(ol);
   }
   section.append(ol);
 }
 
-function login() {
-  console.log(req.readyState+':'+req.status);
-  if (req.readyState == 4 && req.status == 200) {
-    var res = JSON.parse(req.responseText);
-    console.log(res);
-    players = res.players;
-    buildLogins();
-    $('#welcome').hide();
-    $('#waiting').show();
-  }
-}
-
 //-----Start-----
 function buildGameboard(size) {
-  if (size > 26) console.log('WARNING! SIZES LARGER THAN 26 COULD LEAD TO POOR PERFORMANCE');;
+  if (size > 26) console.log('WARNING! SIZES LARGER THAN 26 COULD LEAD TO POOR PERFORMANCE');
   if (size < 12) size = 12;
 
   var section = $('#gameboard');
   section.empty();
-  var table = document.createElement('table')
-  var tr = document.createElement('tr');
-  var td = document.createElement('td');
-  td.setAttribute('class','coords');
-  tr.appendChild(td);
+  var table = $(document.createElement('table'));
+  var tr = $(document.createElement('tr')).appendTo(table);
+  $(document.createElement('td')).attr('class','coords').appendTo(tr);
   for (var i = 1; i < size+1; i++) {
-    td = document.createElement('td');
-    td.setAttribute('class','coords');
-    var num = document.createTextNode(i);
-    td.appendChild(num);
-    tr.appendChild(td);
+    var td = $(document.createElement('td')).addClass('coords').text(i).appendTo(tr);
   }
-  table.appendChild(tr);
+  table.append(tr);
   for (var i = 0; i < size; i++) {
-    tr = document.createElement('tr');
-    td = document.createElement('td');
-    td.setAttribute('class','coords');
-    var letter = document.createTextNode(String.fromCharCode(i+65));
-    td.appendChild(letter);
-    tr.appendChild(td);
+    tr = $(document.createElement('tr'));
+    $(document.createElement('td')).addClass('coords').text(String.fromCharCode(i+65)).appendTo(tr);
     for (var j = 0; j < size; j++) {
-      var td = document.createElement('td');
-      td.setAttribute('id',(String.fromCharCode(i+65)+(j+1)));
-      td.setAttribute('class','game_square');
-      var water = document.createTextNode('~');
-      td.appendChild(water);
-      tr.appendChild(td);
+      $(document.createElement('td')).attr('id',String.fromCharCode(i+65)+(j+1)).addClass('game_square').text('~').appendTo(tr);
     }
-    table.appendChild(tr);
+    table.append(tr);
   }
   section.append(table);
 }
 
-function buildScoreboard() {
-
+function buildScoreboard(players) {
+  var scoreboards = $('.scoreboard');
+  scoreboards.empty();
+  var table = $(document.createElement('table'));
+  for (player of players) {
+    // console.log(player);
+    var tr = $(document.createElement('tr'));
+    $(document.createElement('td')).text(player.name).appendTo(tr);
+    $(document.createElement('td')).text(player.points).appendTo(tr);
+    table.append(tr);
+  }
+  scoreboards.append(table);
 }
 
-function buildAttack() {
+function buildAttack(size) {
+  if (size < 12) size = 12;
+  var section = $('#attack');
+  section.empty();
 
+  var form = $(document.createElement('form')).attr('id','attack');
+  var select = $(document.createElement('select')).attr('id','letter_select');
+  for (var i = 0; i < size; i++) {
+    var letter = String.fromCharCode(i+65);
+    var option = $(document.createElement('option')).val(letter).text(letter).appendTo(select);
+  }
+  form.append(select);
+
+  select = $(document.createElement('select')).attr('id','number_select');
+  for (var i = 1; i < size+1; i++) {
+    var option = $(document.createElement('option')).val(i).text(i).appendTo(select);
+  }
+  form.append(select);
+
+  $(document.createElement('input')).attr({'id': 'fire_button', 'value': 'Fire!', 'type': 'submit'}).appendTo(form);
+  section.append(form);
 }
 
-function start() {
-  // var res = JSON.parse(req.responseText);
-  buildGameboard(15);
-  $('#waiting').hide();
-  $('#game').show();
-}
-
-//-----jQuery-----
+//=====jQuery=====
 $(function() {
+  //-----Login-----
   $('#login').submit(function() {
     var user = {
       name: $('#username').val(),
@@ -97,22 +89,55 @@ $(function() {
     if (!user.name) {
       $('#required').show();
     } else {
-      console.log(user);
+      // console.log(user);
       $.post('/login', user, function(data, status) {
-        var res = JSON.parse(data);
-        console.log(res);
-        players = res.players;
-        buildLogins();
+        res = JSON.parse(data);
+        console.log('login',res);
+        buildLogins(res.players);
         $('#welcome').hide();
         $('#waiting').show();
       });
     }
   });
 
+  //-----Start-----
   $('#start').submit(function() {
-    req = new XMLHttpRequest();
-    req.onreadystatechange = start;
-    req.open('post', '/start', true);
-    req.send();
+    $.post('/start', function(data, status) {
+      res = JSON.parse(data);
+      console.log('start',res);
+      buildGameboard(res.size);
+      buildScoreboard(res.players);
+      buildAttack(res.size);
+      $('#waiting').hide();
+      $('#game').show();
+      game = setInterval(pingGame, 3000);
+    });
+  });
+
+  //-----Game-----
+  function pingGame() {
+    console.log('syn');
+    $.post('/game', function(data, status) {
+      console.log('ack');
+      res = JSON.parse(data);
+      console.log('game',res);
+      if (res.dead) {
+        $('#died').show();
+        $('#fire_button').attr('disabled','');
+      } else {
+        $('#died').hide();
+        $('#fire_button').removeAttr('disabled');
+      }
+    });
+  }
+
+  //-----Attack-----
+  $('#attack').submit(function() {
+    console.log('kaboom!');
+    $.post('/attack',function(data, status) {
+      res = JSON.parse(data);
+      console.log('attack',res);
+      if (res.dead) $('#died').show();
+    });
   });
 });
--- server.js
+++ server.js
@@ -6,8 +6,11 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//-----Variables-----
+// var board = [][];
 var players = [];
 
+//-----Functions-----
 function calcResult() {
   return {
     players: players,
@@ -22,6 +25,7 @@ function calcResult() {
   };
 }
 
+//-----Send Files-----
 app.get('/', function(req, res) {
   console.log("Someone's here!");
   res.sendFile(__dirname + '/warship.html');
@@ -31,16 +35,17 @@ app.get('/style.css', function(req, res) {
   res.sendFile(__dirname + '/style.css');
 });
 
-app.get('/script.js', function(req, res) {
-  res.sendFile(__dirname + '/script.js');
-});
+// app.get('/script.js', function(req, res) {
+//   res.sendFile(__dirname + '/script.js');
+// });
 
 app.get('/main.js', function(req, res) {
   res.sendFile(__dirname + '/main.js');
 });
 
+//-----AJAX Requests-----
 app.post('/login', function(req, res) {
-  console.log(req.body);
+  // console.log(req.body);
   console.log(req.body.name,'joined the game');
   players.push(req.body);
   var info = {
@@ -50,32 +55,28 @@ app.post('/login', function(req, res) {
   res.send(JSON.stringify(info));
 });
 
-app.post('/game', function(req, res) {
-  var result = {
-    players: players,
-    layout: {
-      friend: [{front: 'A1', back: 'A5'},{front: 'B4', back: 'B7'}],
-      damage: ['A3','A4'],
-      hit: ['E7','A12','G2'],
-      miss: ['G1','G3']
-    },
-    dead: false,
-    done: false
+app.post('/start', function(req, res) {
+  console.log('starting game');
+  var game = {
+    size: 15,
+    players: players
   };
+  res.send(JSON.stringify(game));
+});
+
+app.post('/game', function(req, res) {
+  // console.log('syn-ack');
+  var result = calcResult();
 
   res.send(JSON.stringify(result));
 });
 
 app.post('/attack', function(req, res) {
-  console.log('boom');
+  console.log('sploosh~');
   var result = calcResult();
 
-  console.log('result:',result);
+  // console.log('result:',result);
   res.send(JSON.stringify(result));
-});
-
-app.post('/start', function(req, res) {
-  console.log('starting game');
 });
 
 app.post('/restart', function(req, res) {
