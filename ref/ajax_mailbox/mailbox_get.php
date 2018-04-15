9b89622ba639e25b5d95fb74b7b175d3da441eea
Spencer Dent <spencer.dent@covenant.edu>
2018-04-15 19:23:20 -0400

    -unusedPhp

--- ref/ajax_mailbox/mailbox_get.php
+++
@@ -1,9 +0,0 @@
-<?php
-	session_id('JohnHuntMailbox');
-	if( !isset( $_SESSION ) ) {
-		session_start();
-	}
-	$mailboxes = $_SESSION['Mailboxes'];
- 	echo json_encode($mailboxes);
-	//print_r($mailboxes);
-?>
