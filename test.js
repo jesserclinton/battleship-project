64f6e71030fbec896a84ee8fa64ccc4cbb9ba2f7
Spencer Dent <spencer.dent@covenant.edu>
2018-04-20 08:43:41 -0400

    clean-up

---
+++ test.js
@@ -0,0 +1,36 @@
+//-----Scoreboard-----
+function Entry(id, name, score) {
+  this.id = id;
+  this.name = name;
+  this.score = score;
+}
+
+function Scoreboard() {
+  this.entries = [];
+
+  this.addEntry = function(id, name) {
+    for (entry of this.entries) if (entry.id == id) return false;
+    this.entries.push(new Entry(id, name, 0));
+    return true;
+  }
+
+  this.incrementScore = function(id) {
+    for (entry of this.entries) {
+      if (entry.id == id) {
+        entry.score++;
+        return true;
+      }
+    }
+    return false;
+  }
+
+  this.addToScore = function(id, points) {
+    for (entry of this.entries) {
+      if (entry.id == id) {
+        entry.score += points;
+        return true;
+      }
+    }
+    return false;
+  }
+}
